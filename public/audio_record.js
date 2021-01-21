914460fd7051d7cab8b8aa90dc555e8a7dddf11a
Xiao Xiao <xiaosquared@gmail.com>
2021-01-21 20:05:40 +0100

    basic functionality done

--- public/audio_record.js
+++ public/audio_record.js
@@ -1,63 +1,213 @@
-let button;
 let mic;
 let soundRec;
 let soundFile;
 
+let title;
+let id_input;
+let id_label;
+let id;
+let submit_id_button;
+
+let phrase_label;
+
+let play_phrase_button;
+let record_button;
+let play_recorded_button;
+let upload_button;
+
+let phrase;
+let phrase_audio;
+
+let phrases_to_record;
+let currrent_phrase_index;
+
+function preload() {
+  soundFormats("mp3");
+}
 
 function setup() {
+  createFirstPage();
+}
+
+function createFirstPage() {
+  title = createElement("h1", "Record audio reproductions");
+
+  id_label = createP("Subject #:");
+  id_input = createInput('');
+
+  submit_id_button = createButton("Submit");
+  submit_id_button.mouseClicked((mouseEvent)=>{
+    id = id_input.value();
+    sendSubjectID(id_input.value());
+  });
+}
+
+function clearFirstPage() {
+  //title.remove();
+  id_input.remove();
+  submit_id_button.remove();
+  console.log(id_label);
+  id_label.elt.innerHTML = "Subject #: " + id;
+}
+
+// Send id to server. Get list of phrases back. Pick a phrase as the first
+async function sendSubjectID(to_send) {
+  const data = {id: to_send};
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }
+  const response = await fetch('/subject', options);
+  phrases_to_record = await response.json();
+  current_phrase_index = floor(random(phrases_to_record.length));
+  phrase = phrases_to_record[current_phrase_index];
+  phrase_audio = loadSound("data/" + phrase + ".mp3", function() {
+    console.log("successfully loaded " + phrase);
+    play_phrase_button.style('background-color', 'green');
+  });
+
+  clearFirstPage();
+  createRecordingInterface();
+}
+
+function createRecordingInterface() {
+  phrase_label = createP("Phrases remaining: " + phrases_to_record.length);
+
   mic = new p5.AudioIn();
   mic.start();
   soundRec = new p5.SoundRecorder();
   soundRec.setInput(mic)
   soundFile = new p5.SoundFile();
 
-  button = createDiv("");
-  button.position(100,100);
-  button.size(100,100);
-  button.style('background-color', 'grey');
+  createP();
 
+  play_phrase_button = createDiv("Play phrase");
+  play_phrase_button.size(100, 50);
+  play_phrase_button.style('background-color', 'green');
+  play_phrase_button.mouseClicked((mouseEvent)=>{
+    phrase_audio.play();
+  });
 
-  button.mouseClicked((mouseEvent)=>{
+  createP();
+
+  record_button = createDiv("Record voice");
+  record_button.size(100,50);
+  record_button.style('background-color', 'grey');
+
+  record_button.mouseClicked((mouseEvent)=>{
     if (getAudioContext().state != 'running') {
       userStartAudio();
     }
 
-    console.log("recording....");
-    soundRec.record(soundFile); // set up the soundfile to record and start recording
+    if (soundRec.recording == false) {
+      console.log("Start recording");
+      record_button.style('background-color', 'red');
 
-    let recordingTimer = setTimeout(()=>{ // setup a timeout for the recording, after the time below expires, do the tings inside the {}
+      soundRec.record(soundFile);
+    }
 
-      soundRec.stop(); // stop recording
-      let soundBlob = soundFile.getBlob(); //get the recorded soundFile's blob & store it in a variable
+    else {
+      console.log("Stop recording");
+      record_button.style('background-color', 'grey');
 
-      let formdata = new FormData() ; //create a from to of data to upload to the server
-      formdata.append('soundBlob', soundBlob,  'myfiletosave.wav') ; // append the sound blob and the name of the file. third argument will show up on the server as req.file.originalname
+      // change playback button to orange to show that it can be used
+      play_recorded_button.style('background-color', 'orange');
+      soundRec.stop();
+    }
+  });
 
-          // Now we can send the blob to a server...
-      var serverUrl = '/upload'; //we've made a POST endpoint on the server at /upload
-      //build a HTTP POST request
-      var httpRequestOptions = {
-        method: 'POST',
-        body: formdata , // with our form data packaged above
-        headers: new Headers({
-          'enctype': 'multipart/form-data' // the enctype is important to work with multer on the server
-        })
-      };
-      // console.log(httpRequestOptions);
-      // use p5 to make the POST request at our URL and with our options
-      httpDo(
-        serverUrl,
-        httpRequestOptions,
-        (successStatusCode)=>{ //if we were successful...
-          console.log("uploaded recording successfully: " + successStatusCode)
-        },
-        (error)=>{console.error(error);}
-      )
-      console.log('recording stopped');
+  createP();
 
-    },1000) //record for one second
+  play_recorded_button = createDiv("Play recorded");
+  play_recorded_button.size(100, 50);
+  play_recorded_button.style('background-color', 'grey');
+  play_recorded_button.mouseClicked((mouseEvent)=>{
+    if (soundRec.recording) {
+      soundRec.stop();
+      record_button.style('background-color', 'grey');
+    }
+    if (soundFile.isLoaded() && !soundFile.isPlaying()) {
+      upload_button.style('background-color', 'pink');
+      soundFile.play();
+    }
+  });
 
-  }) // close mouseClicked handler
+  createP();
 
+  upload_button = createDiv("Upload File");
+  upload_button.size(100, 50);
+  upload_button.style('background-color', 'grey');
+  upload_button.mouseClicked((mouseEvent)=>{
+    console.log("Upload file");
 
-} //close setup()
+    let soundBlob = soundFile.getBlob(); //get the recorded soundFile's blob & store it in a variable
+    let formdata = new FormData(); // create data to upload to the server
+
+    let filename = phrase + "-s" + id;
+    formdata.append('soundBlob', soundBlob, filename); // append the sound blob and the name of the file. third argument will show up on the server as req.file.originalname
+
+    // Now send blob to server
+    let serverUrl = '/upload';
+    let httpRequestOptions = {
+      method: 'POST',
+      body: formdata,
+      headers: new Headers({
+        'enctype': 'multipart/form-data' // the enctype is important to work with multer on the server
+      })
+    };
+
+    // use p5 to make the POST request at our URL and with our options
+    httpDo(
+      serverUrl,
+      httpRequestOptions,
+      (successStatusCode)=>{ //if we were successful...
+        console.log("uploaded recording successfully: " + successStatusCode);
+
+        initNewPhrase();
+      },
+      (error)=>{console.error(error);}
+    )
+
+  });
+
+  function initNewPhrase() {
+    // clear the phrase audio
+    phrase_audio = null;
+    // remove the phrase we just recorded from the list to record
+    phrases_to_record.splice(current_phrase_index, 1);
+
+    if (phrases_to_record.length == 0) {
+      clearRecordingInterface();
+      createP("DONE!");
+    }
+
+    // pick a new phrase to record
+    current_phrase_index = floor(random(phrases_to_record.length));
+    // change label of how many phrases are left:
+    phrase_label.elt.innerHTML = "Phrases remaining: " + phrases_to_record.length;
+
+    // disable all buttons
+    play_phrase_button.style('background-color', 'grey');
+    record_button.style('background-color', 'grey');
+    play_recorded_button.style('background-color', 'grey');
+    upload_button.style('background-color', 'grey');
+
+    // load new phrase
+    phrase = phrases_to_record[current_phrase_index];
+    phrase_audio = loadSound("data/" + phrase + ".mp3", function() {
+      console.log("successfully loaded " + phrase);
+      play_phrase_button.style('background-color', 'green');
+    });
+  }
+
+  function clearRecordingInterface() {
+    play_phrase_button.remove();
+    record_button.remove();
+    play_recorded_button.remove();
+    upload_button.remove();
+    id_label.remove();
+    phrase_label.remove();
+  }
+
+}
--- server.js
+++ server.js
@@ -3,16 +3,59 @@ const app = express(); //invoke express
 const multer  = require('multer') //use multer to upload blob data
 const upload = multer(); // set multer to be the upload variable (just like express, see above ( include it, then use it/set it up))
 const fs = require('fs'); //use the file system so we can save files
+const glob = require('glob'); // find files with wildcard matching
+
+
+app.use(express.json(({limit: '1mb'})));
+app.post('/subject', (request, response)=> {
+  console.log("Got subject id: " + request.body.id);
+
+  // get available phrases
+  let phrases = glob.sync('public/data/*.mp3', {});
+  let phrases_to_record = [];
+  phrases.forEach((item, index) => {
+    let item2 = item.split('/');
+    phrases_to_record.push(item2[item2.length-1].split('.')[0]);
+  });
+
+  const path = 'public/uploads/s'+request.body.id;
+  if (fs.existsSync(path)) {
+    // Subject's folder already exists
+    // Look at which phrases have already been recorded and remove them from phrase_names
+    let my_recordings = glob.sync(path+'/*.wav', {});
+    phrases_to_record.forEach((item, index) => {
+      let isRecorded = alreadyRecorded(item, my_recordings);
+      if (isRecorded) {
+        phrases_to_record.splice(index, 1);
+      }
+    });
+  } else {
+    // New subject. Create folder to store files
+    fs.mkdirSync(path, 0744);
+  }
+  response.json(phrases_to_record);
+});
+
+// Helper function to see which phrases have already been recorded
+function alreadyRecorded(phrase, my_recordings) {
+  for (let i = 0; i < my_recordings.length; i++) {
+    if (my_recordings[i].includes(phrase)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 app.post('/upload', upload.single('soundBlob'), function (req, res, next) {
   // console.log(req.file); // see what got uploaded
+  let subject = req.file.originalname.split("-")[1]
 
-  let uploadLocation = __dirname + '/public/uploads/' + req.file.originalname // where to save the file to. make sure the incoming name has a .wav extension
+  // where to save the file to. make sure the incoming name has a .wav extension
+  let uploadLocation = __dirname + '/public/uploads/' + subject + "/" + req.file.originalname + ".wav";
 
   fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
   res.sendStatus(200); //send back that everything went ok
-
-})
+});
 
 //serve out any static files in our public HTML folder
 app.use(express.static('public'))
@@ -20,4 +63,4 @@ app.use(express.static('public'))
 //makes the app listen for requests on port 3000
 app.listen(3000, function(){
   console.log("app listening on port 3000!")
-})
+});
